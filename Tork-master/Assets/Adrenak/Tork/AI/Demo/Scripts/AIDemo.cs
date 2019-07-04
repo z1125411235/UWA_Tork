3eda8f3992029cfc29e3784781a2f1b8bfb40f6d
VIVI <z1125411235@gmail.com>
2019-07-04 16:59:04 +0800

    新增 車輛系統 檔案

---
+++ Tork-master/Assets/Adrenak/Tork/AI/Demo/Scripts/AIDemo.cs
@@ -0,0 +1,19 @@
+﻿using Adrenak.Tork;
+using UnityEngine;
+
+namespace Adrenak.Tork.Demo {
+	public class AIDemo : MonoBehaviour {
+		public Vehicle vehicle;
+		public SmoothFollow follow;
+		public Transform destination;
+
+		void Start() {
+			var ai = vehicle.gameObject.AddComponent<AIPlayer>();
+			vehicle.SetPlayer(ai);
+
+			ai.destination = destination;
+
+			follow.target = vehicle.transform;
+		}
+	}
+}
---
+++ Tork-master/Assets/Adrenak/Tork/Physics/Demo/Scripts/PhysicsDemo.cs
@@ -0,0 +1,16 @@
+﻿using Adrenak.Tork;
+using UnityEngine;
+
+namespace Adrenak.Tork.Demo {
+	public class PhysicsDemo : MonoBehaviour {
+		public Vehicle vehicle;
+		public SmoothFollow smoothFollow;
+
+		void Start () {
+			var player = vehicle.gameObject.AddComponent<KeyboardPlayer>();
+			vehicle.SetPlayer(player);
+
+			smoothFollow.target = vehicle.transform;
+		}
+	}
+}
